2c354a6c19fd2b41e5f52a860f82d26b92ed1fac
Wojciech Jelen <wjelen30@gmail.com>
2024-02-04 15:39:03 +0100

    feat: :lipstick: user profile component and nvbar adjustments

--- src/app/components/navbar/navbar.tsx
+++ src/app/components/navbar/navbar.tsx
@@ -2,23 +2,25 @@
 import { useAppSelector } from "@/lib/hooks";
 import { GoogleSignInButton } from "../google-sign-in-button";
 import { SignOut } from "../sign-out-button";
+import Link from "next/link";
+import { UserProfile } from "./user-profile/user-profile";
+import styles from "./navbar.module.scss";
 
 export const Navbar = () => {
   const user = useAppSelector((state) => state.auth.user);
 
-  console.log("#### Navbar user", user);
-
-  if (!user) {
-    return (
-      <div>
-        <GoogleSignInButton />
-      </div>
-    );
-  }
-
   return (
-    <div>
-      <SignOut />
+    <div className={styles.navbar}>
+      <ul>
+        <li>
+          <Link href="/dashboard">Dashboard</Link>
+        </li>
+        <li>
+          <Link href="/history">History</Link>
+        </li>
+      </ul>
+      {user && <UserProfile user={user} />}
+      <div>{user ? <SignOut /> : <GoogleSignInButton />}</div>
     </div>
   );
 };
---
+++ src/app/components/navbar/user-profile/user-profile.tsx
@@ -0,0 +1,23 @@
+import { User } from "next-auth";
+import Image from "next/image";
+
+import styles from "./user-profile.module.scss";
+
+type PropsType = {
+  user: User;
+};
+
+export const UserProfile = ({ user }: PropsType) => {
+  return (
+    <div className={styles.profileWrapper}>
+      <Image
+        src={`${user?.image}`}
+        alt={`${user?.name} profile picture`}
+        width={40}
+        height={40}
+        className={styles.profilePicture}
+      />
+      <div>{`${user?.name}`}</div>
+    </div>
+  );
+};
